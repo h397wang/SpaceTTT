6f4cdf2c2859a19166c942331e7581c62b5d7508
Jerry Li <jerryli97@outlook.com>
2016-01-23 23:06:19 -0500

    added snapping, changing turns

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,227 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class Main : MonoBehaviour {
+
+    Controller controller;
+    GameObject cube;
+    GameObject sphere;
+    GameObject box;
+    GameObject handController;
+    int[,,] boardArray = new int[4, 4, 4];
+    public string xStr;
+    public string yStr;
+    public string zStr;
+
+    public int curPlayer = 1;
+
+
+    // Use this for initialization
+    void Start () {
+        cube = GameObject.Find("Cube");
+        box = GameObject.Find("Box");
+        sphere = GameObject.Find("Sphere");
+        handController = GameObject.Find("HandController");
+        controller = new Controller();
+        controller.EnableGesture(Gesture.GestureType.TYPE_KEY_TAP);
+        //controller.Config.SetFloat("Gesture.KeyTap.MinDownVelocity", 30.0f);
+        controller.Config.SetFloat("Gesture.KeyTap.HistorySeconds", 0.2f);
+        //controller.Config.SetFloat("Gesture.KeyTap.MinDistance", 0.6f);
+
+        /*
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                for (int k = 0; k <4; k++)
+                {
+                    SpawnObject(1, (i + 1) * 0.6f - 1.5f, (j + 1) * 0.6f - 1.5f, (k + 1) * 0.6f - 1.5f);
+                }
+            }
+        }
+        */
+    }
+
+
+    public Vector3 snapPos;
+    Frame lastFrame = Frame.Invalid;
+    public GameObject lastMove;
+    // Update is called once per frame
+    void Update () {
+        Frame frame = controller.Frame();
+        GestureList gestures = frame.Gestures(lastFrame);
+        lastFrame = frame;
+        /*
+        for (int i = 0; i < gestures.Count; i++)
+        {
+            Gesture gesture = gestures[i];
+            if (gesture.Type == Gesture.GestureType.TYPE_KEY_TAP && gesture.Hands[0].IsRight)
+            {   
+                KeyTapGesture tap = new KeyTapGesture(gesture);
+                GameObject newCube = Instantiate(cube);
+                newCube.transform.position = box.transform.position;
+                newCube.transform.rotation = box.transform.rotation;
+                newCube.transform.parent = box.transform;
+                
+                Vector tapPosition = tap.Position;
+                Vector3 correctedPos = handController.transform.TransformPoint(tapPosition.ToUnityScaled());
+                //correctedPos.y = correctedPos.y - 1.5f;
+                //correctedPos.z = correctedPos.z - 0.3f;
+                //newCube.transform.position = SnapToGrid(correctedPos);
+                snapPos = SnapToGrid(correctedPos);
+                SpawnObject(curPlayer, snapPos);
+                Vector3 arrayPos = UnityToArrayIndex(snapPos);
+                xStr = "X: " + ((int)arrayPos.x).ToString();
+                yStr = "Y: " + ((int)arrayPos.y).ToString();
+                zStr = "Z: " + ((int)arrayPos.z).ToString();
+
+                Debug.Log(correctedPos + " | " + snapPos);
+            }
+        }       */
+
+        //Find the current position of the index finger and display it in a text box
+
+        HandList hands = frame.Hands;
+        Vector3 currentPos;
+        Vector tipPos;
+        foreach (Hand hand in hands)
+        {
+            if (hand.IsRight)
+            {
+                foreach (Finger finger in hand.Fingers)
+                {
+                    if ( (int)finger.Type == 1)
+                    {
+                        for (int i = 0; i < gestures.Count; i++)
+                        {
+                            Gesture gesture = gestures[i];
+                            if (gesture.Type == Gesture.GestureType.TYPE_KEY_TAP && gesture.Hands[0].IsRight)
+                            {
+                                KeyTapGesture tap = new KeyTapGesture(gesture);
+                                /*newCube.transform.position = box.transform.position;
+                                newCube.transform.rotation = box.transform.rotation;
+                                newCube.transform.parent = box.transform;
+                                */
+                                Vector tapPosition = tap.Position;
+                                Vector3 correctedPos = handController.transform.TransformPoint(tapPosition.ToUnityScaled());
+                                //correctedPos.y = correctedPos.y - 1.5f;
+                                //correctedPos.z = correctedPos.z - 0.3f;
+                                //newCube.transform.position = SnapToGrid(correctedPos);
+                                snapPos = SnapToGrid(correctedPos);
+                                if (snapPos.x > 3f || snapPos.y > 3f || snapPos.z > 3f)
+                                SpawnObject(curPlayer, snapPos);
+                                Vector3 arrayPos = UnityToArrayIndex(snapPos);
+                            }
+                        }
+
+                        tipPos = finger.TipPosition;
+                        currentPos = handController.transform.TransformPoint(tipPos.ToUnityScaled());
+                        snapPos = SnapToGrid(currentPos);
+                        Vector3 gridPos = UnityToArrayIndex(snapPos);
+                        Debug.Log(snapPos + "   |   " + gridPos);
+                        xStr = "X: " + ((int)gridPos.x).ToString();
+                        yStr = "Y: " + ((int)gridPos.y).ToString();
+                        zStr = "Z: " + ((int)gridPos.z).ToString();
+                    }
+
+                    
+                }
+            }
+
+        }
+
+        
+    }
+
+    void SpawnObject(int player, Vector3 position)
+    {
+        GameObject newObject;
+        if (player == 1)
+        {
+            newObject = Instantiate(cube);
+            curPlayer = 2;
+        }
+        else
+        {
+            newObject = Instantiate(sphere);
+            curPlayer = 1;
+        }
+        newObject.transform.parent = box.transform;
+        newObject.transform.position = position;
+    }
+
+    public Vector3 UnityToArrayIndex(Vector3 pv) {
+        // Function inputs are the Unity coordinates of a point
+        // Maps and outputs the corresponding array coordinate (integer)
+
+        int x = (int)(5.2 / 3 * (pv.x + 0.9));
+        int y = (int)(5.2 / 3 * (pv.y + 0.9));
+        int z = (int)(5.2 / 3 * (- pv.z + 0.9));
+
+        Vector3 output = new Vector3(x, y, z);
+        return output;
+    }
+
+    public Vector3 SnapToGrid(Vector3 tapPosition)
+    {
+        float x = tapPosition.x;
+        float y = tapPosition.y;
+        float z = tapPosition.z;
+        float newX = 0;
+        float newY = 0;
+        float newZ = 0;
+
+        if (-1.4f < x && x < -0.6f)
+            newX = -0.9f;
+        else if (-0.6f < x && x < 0f)
+            newX = -0.3f;
+        else if (0f < x && x < 0.6f)
+            newX = 0.3f;
+        else if (0.6f < x && x < 1.2f)
+            newX = 0.9f;
+        else
+            newX = 50f;
+
+        if (-1.4f < y && y < -0.6f)
+            newY = -0.9f;
+        else if (-0.6f < y && y < 0f)
+            newY = -0.3f;
+        else if (0f < y && y < 0.6f)
+            newY = 0.3f;
+        else if (0.6f < y && y < 1.2f)
+            newY = 0.9f;
+        else
+            newY = 50f;
+
+        if (-1.4f < z && z < -0.6f)
+            newZ = -0.9f;
+        else if (-0.6f < z && z < 0f)
+            newZ = -0.3f;
+        else if (0f < z && z < 0.6f)
+            newZ = 0.3f;
+        else if (0.6f < z && z < 1.2f)
+            newZ = 0.9f;
+        else newZ = 50f;
+
+        Vector3 newPosition = new Vector3(newX, newY, newZ);
+
+        return newPosition;
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(50, 50, 50, 50),xStr + 
+            "\n" + yStr + "\n" + zStr);
+    }
+
+    void Undo(GameObject lastObject)
+    {
+        Destroy(lastObject);
+        if (curPlayer == 1)
+            curPlayer = 2;
+        else
+            curPlayer = 1;
+    }
+
+}
