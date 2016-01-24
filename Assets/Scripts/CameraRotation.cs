2402be4e9c2efe7eed9f5d8424ecd225393e94e5
Jerry Li <jerryli97@outlook.com>
2016-01-23 20:54:23 -0500

    first commit

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using Leap;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    /// <summary>
+    /// The Leap controller.
+    /// </summary>
+    Controller controller;
+    
+    /// <summary>
+    /// The current frame captured by the Leap Motion.
+    /// </summary>
+    Frame CurrentFrame
+    {
+        get { return (IsReady) ? controller.Frame() : null; }
+    }
+
+    /// <summary>
+    /// Gets the hands data captured from the Leap Motion.
+    /// </summary>
+    /// <value>
+    /// The hands data captured from the Leap Motion.
+    /// </value>
+    HandList Hands
+    {
+        get { return (CurrentFrame != null && CurrentFrame.Hands.Count > 0) ? CurrentFrame.Hands : null; }
+    }
+
+    /// <summary>
+    /// Gets a value indicating whether the Leap Motion is ready.
+    /// </summary>
+    /// <value>
+    ///   <c>true</c> if this instance is ready; otherwise, <c>false</c>.
+    /// </value>
+    bool IsReady
+    {
+        get { return (controller != null && controller.Devices.Count > 0 && controller.IsConnected); }
+    }
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        controller = new Controller();
+    }
+
+    /// <summary>
+    /// Update function called every frame.
+    /// </summary>
+    float curYaw;
+    float curYRotation;
+    void Update()
+    {
+        Hand leftHand; // The front most hand captured by the Leap Motion Controller
+
+        // Check if the Leap Motion Controller is ready
+        if (!IsReady || Hands == null)
+        {
+            return;
+        }
+        leftHand = Hands.Leftmost;
+        if (leftHand.IsLeft && leftHand.Direction.Yaw > curYaw && leftHand.Direction.Yaw > 0.1)
+        {
+            curYRotation += leftHand.Direction.Yaw * 15;
+            transform.rotation = Quaternion.Euler(0, curYRotation, 0);
+        }
+        //For relative orientation
+        //transform.rotation *= Quaternion.Euler( leftHand.Direction.Pitch, leftHand.Direction.Yaw, leftHand.PalmNormal.Roll );
+        curYaw = leftHand.Direction.Yaw;
+
+ 
+
+        
+    }
+
+    
+
+}
---
+++ Assets/Scripts/GestureControl.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class GestureControl : MonoBehaviour {
+
+    Controller controller;
+    GameObject cube;
+    GameObject box;
+
+    // Use this for initialization
+    void Start () {
+        cube = GameObject.Find("Cube");
+        box = GameObject.Find("Box");
+        controller = new Controller();
+        controller.EnableGesture(Gesture.GestureType.TYPE_KEY_TAP);
+        //controller.Config.SetFloat("Gesture.KeyTap.MinDownVelocity", 30.0f);
+        controller.Config.SetFloat("Gesture.KeyTap.HistorySeconds", 0.2f);
+        //controller.Config.SetFloat("Gesture.KeyTap.MinDistance", 0.6f);
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                for (int k = 0; k <4; k++)
+                {
+                    SpawnObject(1, (i + 1) * 0.6f - 1.5f, (j + 1) * 0.6f - 1.5f, (k + 1) * 0.6f - 1.5f);
+                }
+            }
+        }
+    }
+    Frame lastFrame = Frame.Invalid;
+    // Update is called once per frame
+    void Update () {
+        Frame frame = controller.Frame();
+        GestureList gestures = frame.Gestures(lastFrame);
+        lastFrame = frame;
+
+        for (int i = 0; i < gestures.Count; i++)
+        {
+            Gesture gesture = gestures[i];
+            if (gesture.Type == Gesture.GestureType.TYPE_KEY_TAP && gesture.Hands[0].IsRight)
+            {
+                KeyTapGesture tap = new KeyTapGesture(gesture);
+                GameObject newCube = Instantiate(cube);
+                newCube.transform.parent = box.transform;
+                Vector tapPosition = tap.Position;
+                newCube.transform.position = tapPosition.ToUnityScaled(false) * 10;
+
+                Vector3 correctedPos = newCube.transform.position;
+                correctedPos.y = correctedPos.y - 1.5f;
+                correctedPos.z = correctedPos.z - 0.3f;
+                newCube.transform.position = correctedPos;
+                Debug.Log(correctedPos);
+            }
+        }
+    }
+
+    void SpawnObject(int player, float x, float y, float z)
+    {
+        GameObject newCube = Instantiate(cube);
+        newCube.transform.parent = box.transform;
+        Vector3 position = newCube.transform.position;
+        position.x = x;
+        position.y = y;
+        position.z = z;
+        newCube.transform.position = position;
+    }
+
+}
